9c4fa00c6b41034978c8bb3d3132390d1a5ee315
klego <ladonin05@mail.ru>
2026-02-02 15:44:58 +0300

    logic of the catalog and book buttons

--- handlers/catalog.py
+++ handlers/catalog.py
@@ -1,17 +1,133 @@
 from __future__ import annotations
 from aiogram import F, Router, types
 
-from keyboards.catalog import generate_catalog_kb
+from keyboards.catalog import BookCBData, CategoryCBData, back_to_category_catalog_kb, generate_books_kb, generate_catalog_kb
 
 router = Router()
 
 CATALOG = {
-    "novels": {"text": "Романы", "description": "Книги романы"},
-    "fantasy": {"text": "Фентези", "description": "Книги фентези"},
-    "horror": {"text": "Хорроры", "description": "Книги хорроры"},
-    "detectives": {"text": "Детективы", "description": "Книги детективы"},
-    "documentaries": {"text": "Документалки", "description": "Книги документалки"}
+    "novels": {
+        "text": "Романы",
+        "description": "Книги романы",
+        "books": [
+            {
+                "id": 1,
+                "name": "Книга {}",
+                "description": "Описание книги {}",
+                "price": 100,
+            },
+            {
+                "id": 2,
+                "name": "Книга {}",
+                "description": "Описание книги {}",
+                "price": 200,
+            },
+            {
+                "id": 3,
+                "name": "Книга {}",
+                "description": "Описание книги {}",
+                "price": 300,
+            },
+        ],
+    },
+    "fantasy": {
+        "text": "Фентези",
+        "description": "Книги фентези",
+        "books": [
+            {
+                "id": 4,
+                "name": "Книга {}",
+                "description": "Описание книги {}",
+                "price": 100,
+            },
+            {
+                "id": 5,
+                "name": "Книга {}",
+                "description": "Описание книги {}",
+                "price": 200,
+            },
+            {
+                "id": 6,
+                "name": "Книга {}",
+                "description": "Описание книги {}",
+                "price": 300,
+            },
+        ],
+    },
+    "horror": {
+        "text": "Хорроры",
+        "description": "Книги хорроры",
+        "books": [
+            {
+                "id": 7,
+                "name": "Книга {}",
+                "description": "Описание книги {}",
+                "price": 100,
+            },
+            {
+                "id": 8,
+                "name": "Книга {}",
+                "description": "Описание книги {}",
+                "price": 200,
+            },
+            {
+                "id": 9,
+                "name": "Книга {}",
+                "description": "Описание книги {}",
+                "price": 300,
+            },
+        ],
+    },
+    "detectives": {
+        "text": "Детективы",
+        "description": "Книги детективы",
+        "books": [
+            {
+                "id": 10,
+                "name": "Книга {}",
+                "description": "Описание книги {}",
+                "price": 100,
+            },
+            {
+                "id": 11,
+                "name": "Книга {}",
+                "description": "Описание книги {}",
+                "price": 200,
+            },
+            {
+                "id": 12,
+                "name": "Книга {}",
+                "description": "Описание книги {}",
+                "price": 300,
+            },
+        ],
+    },
+    "documentaries": {
+        "text": "Документалки",
+        "description": "Книги документалки",
+        "books": [
+            {
+                "id": 13,
+                "name": "Книга {}",
+                "description": "Описание книги {}",
+                "price": 100,
+            },
+            {
+                "id": 14,
+                "name": "Книга {}",
+                "description": "Описание книги {}",
+                "price": 200,
+            },
+            {
+                "id": 15,
+                "name": "Книга {}",
+                "description": "Описание книги {}",
+                "price": 300,
+            },
+        ],
+    },
 }
+
 
 @router.callback_query(F.data == "catalog")
 @router.message(F.text == "Каталог")
@@ -28,21 +144,38 @@ async def catalog(update: types.Message | types.CallbackQuery):
         )
 
 
-@router.callback_query(F.data.startswith("category:"))
-async def category_info(callback: types.CallbackQuery):
-    category_key = callback.data.split(":")[-1]
-    category = CATALOG.get(category_key)
+@router.callback_query(CategoryCBData.filter())
+async def category_info(callback: types.CallbackQuery, callback_data: CategoryCBData):
+
+    category = CATALOG.get(callback_data.category)
 
     await callback.message.edit_text(
         text=category["description"],
-        reply_markup=types.InlineKeyboardMarkup(
-            inline_keyboard=[
-                [
-                    types.InlineKeyboardButton(
-                        text="Назад", 
-                        callback_data="catalog"
-                    )
-                ]
-            ]
-        )
+        reply_markup=generate_books_kb(category["books"], callback_data.category),
+    )
+
+
+@router.callback_query(BookCBData.filter())
+async def book_info(callback: types.CallbackQuery, callback_data: BookCBData):
+    book_id = callback_data.id
+    category = CATALOG.get(callback_data.category)
+    
+    book = None
+
+    for bk in category["books"]:
+        if bk["id"] == book_id:
+            book = bk
+            break
+    
+    if not book:
+        return await callback.answer("Книга не найдена.")
+
+    await callback.message.edit_text(
+        text=(
+            f"Название - {book['name'].format(book['id'])}\n"
+            f"Описание - {book['description'].format(book['id'])}\n"
+            f"Цена: {book['price']}руб.\n\n"
+            "Хотите купить эту книгу?"
+        ),
+        reply_markup=back_to_category_catalog_kb(callback_data.category),
     )
--- keyboards/catalog.py
+++ keyboards/catalog.py
@@ -1,4 +1,14 @@
+from aiogram.filters.callback_data import CallbackData
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+class CategoryCBData(CallbackData, prefix="category"):
+    category: str
+
+
+class BookCBData(CallbackData, prefix="book"):
+    id: int
+    category: str
 
 
 def generate_catalog_kb(catalog):
@@ -8,9 +18,40 @@ def generate_catalog_kb(catalog):
         keyboard.inline_keyboard.append(
             [
                 InlineKeyboardButton(
-                    text=category["text"], 
-                    callback_data=f"category:{category_cb}"
+                    text=category["text"],
+                    callback_data=CategoryCBData(category=category_cb).pack(),
                 )
             ]
         )
     return keyboard
+
+
+def generate_books_kb(books, category):
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[])
+
+    for book in books:
+        keyboard.inline_keyboard.append(
+            [
+                InlineKeyboardButton(
+                    text=book["name"].format(book["id"]),
+                    callback_data=BookCBData(id=book["id"], category=category).pack(),
+                )
+            ]
+        )
+    keyboard.inline_keyboard.append(
+        [InlineKeyboardButton(text=" << Назад", callback_data="catalog")]
+    )
+    return keyboard
+
+
+def back_to_category_catalog_kb(category_cb):
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text=" << Назад",
+                    callback_data=CategoryCBData(category=category_cb).pack(),
+                )
+            ]
+        ]
+    )
